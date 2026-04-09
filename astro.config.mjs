7b08e122a546a07a5ee773ae504ef13a9fc74674
Corey Burns <coreyburnsdev@aol.com>
2026-04-09 01:18:26 -0400

    Fix portfolio accessibility and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,4 @@ import icon from "astro-icon";
 export default defineConfig({
   site: "https://coreyburns.ca",
   integrations: [sitemap(), icon()],
-  build: {
-    inlineStylesheets: "always",
-  },
 });
